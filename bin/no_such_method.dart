dc2437c26d6d90ecbec88ed9cbf4494b54954e5d
Mohamad Giri Sundava <m.girisundava206@gmail.com>
2025-01-03 21:48:31 +0700

    add: no such method for abstract class

--- bin/no_such_method.dart
+++ bin/no_such_method.dart
@@ -1,9 +1,10 @@
 import 'data/repository.dart';
 
 void main() {
-  dynamic repository = Repository('products');
+  Repository repository = Repository('products');
 
   repository.id('1');
   repository.name('Laptop');
   repository.quantity(1000);
+  // repository.blabla(1000);
 }
